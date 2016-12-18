931529a49583a8e9f81bcc3568a61333504bf474
Blake G <blakeblind@gmail.com>
2016-12-18 16:06:39 -0700

    Ensured messages are sent with a carriage return, and added tests to check.

--- tcp_server_test.go
+++ tcp_server_test.go
@@ -1,6 +1,7 @@
 package tcp_server
 
 import (
+	"bufio"
 	. "github.com/smartystreets/goconvey/convey"
 	"net"
 	"testing"
@@ -16,8 +17,13 @@ func Test_accepting_new_client_callback(t *testing.T) {
 
 	var messageReceived bool
 	var messageText string
+	var messageTextReceived string
+	messageTest := "testing"
+
 	var newClient bool
-	var connectinClosed bool
+	var connectionClosed bool
+
+	var err error
 
 	server.OnNewClient(func(c *Client) bool {
 		newClient = true
@@ -26,9 +32,10 @@ func Test_accepting_new_client_callback(t *testing.T) {
 	server.OnNewMessage(func(c *Client, message string) {
 		messageReceived = true
 		messageText = message
+		c.Send(message + "\r\r\n\n")
 	})
 	server.OnClientConnectionClosed(func(c *Client, err error) {
-		connectinClosed = true
+		connectionClosed = true
 	})
 	go server.Listen()
 
@@ -40,14 +47,19 @@ func Test_accepting_new_client_callback(t *testing.T) {
 	if err != nil {
 		t.Fatal("Failed to connect to test server")
 	}
-	conn.Write([]byte("Test message\n"))
+	conn.Write([]byte(messageTest + "\n"))
+	messageTextReceived, err = bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		t.Fatal("Failed to receive message.\r\n" + err.Error())
+	}
 	conn.Close()
 
 	// Wait for server
 	time.Sleep(10 * time.Millisecond)
 
 	Convey("Messages should be equal", t, func() {
-		So(messageText, ShouldEqual, "Test message")
+		So(messageText, ShouldEqual, messageTest)
+		So(messageTextReceived, ShouldEqual, messageTest+"\r\n")
 	})
 	Convey("It should receive new client callback", t, func() {
 		So(newClient, ShouldEqual, true)
@@ -56,6 +68,6 @@ func Test_accepting_new_client_callback(t *testing.T) {
 		So(messageReceived, ShouldEqual, true)
 	})
 	Convey("It should receive connection closed callback", t, func() {
-		So(connectinClosed, ShouldEqual, true)
+		So(connectionClosed, ShouldEqual, true)
 	})
 }
